ce65c061cd41058e678040b8e3d9bfb6989d54bf
Justin Valentin <Justinvalentine88@gmail.com>
2017-03-25 21:19:57 -0400

    trying to get employee records to display

--- tiy_database.rb
+++ tiy_database.rb
@@ -1,16 +1,16 @@
 require 'csv'
 
 class Person
-  attr_reader "name", "phone_number", "address", "position", "salary", "slack_account", "github_account"
+  attr_reader "name", "phone", "address", "position", "salary", "slack", "github"
 
-  def initialize(name, phone_number, address, position, salary, slack_account, github_account)
+  def initialize(name, phone, address, position, salary, slack, github)
     @name = name
-    @phone_number = phone_number
+    @phone = phone
     @address = address
     @position = position
     @salary = salary
-    @slack_account = slack_account
-    @github_account = github_account
+    @slack = slack
+    @github = github
   end
 end
 
@@ -19,14 +19,14 @@ class DataBase
     @profiles = []
     CSV.foreach("employees.csv", headers: true) do |row|
       name = row["name"]
-      phone_number = row["phone_number"]
+      phone = row["phone"]
       address = row["address"]
       position = row["postion"]
       salary = row["salary"]
-      slack_account = row["slack_account"]
-      github_account = row["github_account"]
+      slack = row["slack"]
+      github = row["github"]
 
-      person = Person.new(name, phone_number, address, position, salary, slack_account, github_account)
+      person = Person.new(name, phone, address, position, salary, slack, github)
 
       @profiles << person
     end
@@ -44,7 +44,7 @@ class DataBase
       puts "This profile already exists"
     else
       puts "What is your phone number?"
-      phone_number = gets.chomp
+      phone = gets.chomp
 
       puts "What is your address?"
       address = gets.chomp
@@ -53,16 +53,15 @@ class DataBase
       position = gets.chomp
 
       puts "What is your salary?"
-      salary = gets.chomp.to_i
+      salary = gets.chomp
 
       puts "what is your Slack Account?"
-      slack_account = gets.chomp
+      slack = gets.chomp
 
       puts "what is your Git Account?"
-      github_account = gets.chomp
+      github = gets.chomp
 
-      account = Person.new(name, phone_number, address, position, salary, slack_account, github_account)
-      puts "The name for this account is #{account.name} Phone number is #{account.phone_number} you addrress is #{account.address} your current position is #{account.position} your current salary is #{account.salary} yours Slack Account is #{account.slack_account} and your Github Account is #{account.github_account}"
+      account = Person.new(name, phone, address, position, salary, slack, github)
 
       @profiles << account
 
@@ -73,7 +72,7 @@ class DataBase
   def search_person
     puts "Please type in persons name. "
     search_person = gets.chomp
-    found_account = @profiles.find { |person| person.name.include?(search_person) || person.slack_account == search_person || person.github_account == search_person }
+    found_account = @profiles.find { |person| person.name.include?(search_person) || person.slack == search_person || person.github == search_person }
     if found_account
       puts "This is #{found_account.name}'s information.
        \nName: #{found_account.name}
@@ -103,7 +102,7 @@ class DataBase
     CSV.open("employees.csv", "w") do |csv|
       csv << ["name", "phone_number", "address", "position", "salary", "slack_account", "github_account"]
       @profiles.each do |person|
-        csv << [person.name, person.phone_number, person.address, person.position, person.salary, person.slack_account, person.github_account]
+        csv << [person.name, person.phone, person.address, person.position, person.salary, person.slack, person.github]
       end
     end
   end
@@ -122,42 +121,37 @@ class DataBase
   end
 
   def instructor_salary
-    @profiles.select { |person| person.position.include?("Instructor")}.map { |person| person.salary }.sum
+    @profiles.select { |person| person.position.include?("Instructor") }.map { |person| person.salary }.sum
   end
 
   def director_salary
-    @profiles.select { |person| person.position.include?("Campus Director") }.map {|person| person.salary }.sum
+    @profiles.select { |person| person.position.include?("Campus Director") }.map { |person| person.salary }.sum
   end
 
   def total_instructor
-    @profiles.select { |person| person.position.include?("Intructor")}.count
+    @profiles.select { |person| person.position.include? ("Intructor") }.count
   end
 
   def total_director
-    @profiles.select { |person| person.position.include?("Campus Director")}.count
+    @profiles.select { |person| person.position.include?("Campus Director") }.count
   end
 
   def total_students
-    @profiles.select { |person| person.position.include?("Student")}.count
+    @profiles.select { |person| person.position.include?("Student") }.count
   end
 
-  def start
-    choice = ()
-    while choice "!="
-      choice = initial_question
-      if choice == "a"
-        add_person
-      elsif choice == "s"
-        search_person
-      elsif choice == "d"
-        delete_person
-      elsif choice == "e"
-        employee_report
-      else
-        puts "program closing, information saved 👋🏼"
-      end
-    end
+  data = DataBase.new
+
+  loop do
+    puts 'Would you like to Add (A), Search (S) or Delete (D) a person or view the Report (E) from the Iron Yard Database?'
+    selected = gets.chomp.upcase
+
+    data.add_person if selected == 'A'
+
+    data.search_person if selected == 'S'
+
+    data.delete_person if selected == 'D'
+
+    data.employee_report if selected == 'E'
   end
 end
-
-DataBase.new.start
